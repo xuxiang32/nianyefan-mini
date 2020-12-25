1e8088f2914a86c9cd6de044ec1e7257f8960993
黄敏 <min.huang@sirun.net>
2020-12-25 16:03:01 +0800

    公用方法新增

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,6 @@
 import http from './commonRequest.js' // 获取基础数据信息
 const api = {
 	  login: '/vender_tvt/login', // 登陆
-  baseImgUri: 'https://event.1shang.com/links-software-web-file/integralMall/jiangxin/'
   }
 
 export default api
@@ -14,9 +13,3 @@ export function login (parameter) {
     data: parameter
   })
 }
-
-// 图片相对路径
-export function baseImgUri (parameter) {
-  return api.baseImgUri
-}
-

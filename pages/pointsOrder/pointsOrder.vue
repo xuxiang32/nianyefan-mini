6cf80cadda7b43d2b70d291d619cc5cf920c91a6
xuxiang32 <xuxiang32@163.com>
2020-12-27 11:15:33 +0800

    add pointsOrder

---
+++ pages/pointsOrder/pointsOrder.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="points-order">
+		<NavBar :backBtn="true" title="邀请家人组饭局" background="rgba(0,0,0,0)" color="#FFF"></NavBar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+@import '../../static/less/global.less';
+.table-main{
+	width: 100%;
+	height: 100vh;
+	background: ~"url(@{baseImgUrl}bgRed.png) no-repeat center";
+	background-size:cover ;
+	
+}
+</style>
